4b45b1063fbb27aa072aeee82ae4ecfe5cf6b9f1
Wawan <wawannss@gmail.com>
2022-08-19 16:15:33 +0700

    Redux Actions

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -13,6 +13,8 @@ import Contact from './ContactComponent';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
+import { addComment } from '../redux/ActionCreators';
+
 
 const mapStateToProps = state => {
   return {
@@ -22,6 +24,12 @@ const mapStateToProps = state => {
     leaders: state.leaders
   }
 }
+
+const mapDispatchToProps = dispatch => ({
+  
+  addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment))
+
+});
 
 class Main extends Component {
 
@@ -57,10 +65,18 @@ class Main extends Component {
       );
     }
 
+    const mapDispatchToProps = dispatch => ({
+  
+      addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment))
+    
+    });
+    
     const DishWithId = ({match}) => {
       return(
           <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
-            comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+            comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+            addComment={this.props.addComment} 
+            />
       );
     };
 
@@ -83,4 +99,4 @@ class Main extends Component {
   }
 }
 
-export default withRouter(connect(mapStateToProps)(Main));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
